cdd0f2a7b20e276d47392391f04b59f21631d317
Winters67 <emmanuel.vbg@gmail.com>
2022-12-11 15:14:32 +0100

    menu trie css ok

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -58,6 +58,50 @@ async function initInfo() {
   displayData(selectedPhotographer);
 }
 
+// menu trie
+document.querySelectorAll('.custom-select').forEach(setupSelector);
+
+function setupSelector(selector) {
+  selector.addEventListener('change', e => {
+    console.log('changed', e.target.value)
+  })
+
+  selector.addEventListener('mousedown', e => {
+    if(window.innerWidth >= 420) {// override look for non mobile
+      e.preventDefault();
+
+      const select = selector.children[0];
+      const dropDown = document.createElement('ul');
+      dropDown.className = "selector-options";
+
+      [...select.children].forEach(option => {
+        const dropDownOption = document.createElement('li');
+        dropDownOption.textContent = option.textContent;
+
+        dropDownOption.addEventListener('mousedown', (e) => {
+          e.stopPropagation();
+          select.value = option.value;
+          selector.value = option.value;
+          select.dispatchEvent(new Event('change'));
+          selector.dispatchEvent(new Event('change'));
+          dropDown.remove();
+        });
+
+        dropDown.appendChild(dropDownOption);   
+      });
+
+      selector.appendChild(dropDown);
+
+      // handle click out
+      document.addEventListener('click', (e) => {
+        if(!selector.contains(e.target)) {
+          dropDown.remove();
+        }
+      });
+    }
+  });
+}
+
 getData().then((result) => {
   // media
   let listMedia = result.media;
@@ -103,7 +147,7 @@ getData().then((result) => {
   }
 
   let lightBox = new LightBox(listCategoryFilter);
-  const triLikes = document.getElementById("nav-select");
+  const triLikes = document.getElementById("nav-option-select");
 
   triLikes.addEventListener("change", function (e) {
     const element = document.getElementById("photograph-gallery");
@@ -144,24 +188,23 @@ getData().then((result) => {
         });
       });
 
-    function price() {
+    function priceLikes() {
       for (let index = 0; index < listphotographFilter.length; index++) {
         const element = listphotographFilter[index];
-        console.log(element);
+        // console.log(element);
         const result = listCategoryFilter
-        .map((i) => i.likes)
-        .reduce((a, b) => a + b);
-        console.log(result);
-        
+          .map((i) => i.likes)
+          .reduce((a, b) => a + b);
+        // console.log(result);
+
         const para = `<span class="likesInfoPhotographer">
         <p class="likesContainer">${result}<img class="logoLikes" src="/assets/icons/heart-black.svg" alt="logo like" /></p>
         <p>${element.price} € / jour </p> 
       </span>`;
         document.querySelector(".infoPhotographerDisplay").innerHTML = para;
-        
       }
     }
-    price();
+    priceLikes();
   }
   initInfo();
   mediaDisplay();
